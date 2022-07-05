5df544c4b2a210661550641a8fdffe60ae5017fa
Lawrence Li <tl5@illinois.edu>
2022-07-05 18:45:09 -0400

    Exploring two different layouts

--- main.go
+++ main.go
@@ -14,13 +14,13 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "home.html"))))
+		views.Must(views.ParseFS(templates.FS, "home.html", "layout-parts.html"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "contact.html"))))
+		views.Must(views.ParseFS(templates.FS, "contact.html", "layout-parts.html"))))
 
 	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "faq.html"))))
+		views.Must(views.ParseFS(templates.FS, "faq.html", "layout-parts.html"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
