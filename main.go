de06724d685ec286de84b2e7da23bc8a66b54429
Lawrence Li <tl5@illinois.edu>
2022-07-07 16:10:16 -0400

    added a sign up page

--- main.go
+++ main.go
@@ -31,6 +31,12 @@ func main() {
 			"faq.html", "tailwind.html",
 		))))
 
+	r.Get("/signup", controllers.StaticHandler(
+		views.Must(views.ParseFS(
+			templates.FS,
+			"signup.html", "tailwind.html",
+		))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
 	})
