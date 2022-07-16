b7bba90961d2e6a2d692c0ed7e0a923b1864845c
lawrence.li <tiantonl@andrew.cmu.edu>
2022-07-16 12:48:03 -0400

    created a table in sql

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,31 +1,70 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type User struct {
-	Name string
-	Age  int
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lolomart",
+		SSLMode:  "disable",
+	}
+
+	db, err := sql.Open("pgx", cfg.String())
 
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	user := User{
-		Name: "Jon Snow",
-		Age:  23,
-	}
-
-	err = t.Execute(os.Stdout, user)
-
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("Connected！")
+
+	// Create a table
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		email TEXT UNIQUE NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS orders (
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		amount INT,
+		description TEXT
+	);
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Table created！")
 
 }
