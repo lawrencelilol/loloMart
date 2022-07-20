227690444c564ba4632bcabb0ecd600d9a91053b
lawrence.li <tiantonl@andrew.cmu.edu>
2022-07-19 22:09:33 -0400

    adding sample orders

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -84,22 +84,73 @@ func main() {
 
 	// fmt.Println("User created！id =", id)
 
-	id := 4
-	row := db.QueryRow(`
-	SELECT name, email 
-	FROM users 
-	WHERE id = $1;`, id)
+	// id := 4
+	// row := db.QueryRow(`
+	// SELECT name, email
+	// FROM users
+	// WHERE id = $1;`, id)
 
-	var name, email string
+	// var name, email string
 
-	err = row.Scan(&name, &email)
+	// err = row.Scan(&name, &email)
 
-	if err == sql.ErrNoRows {
-		fmt.Println("No row found！")
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("No row found！")
+	// }
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User information: name = %s, email = %s\n", name, email)
+
+	userID := 1
+
+	for i := 0; i <= 5; i++ {
+		amount := i * 100
+		desc := fmt.Sprintf("Fake Order %d", i)
+		_, err = db.Exec(`
+		INSERT INTO orders (user_id, amount, description)
+		VALUES ($1,$2,$3);`, userID, amount, desc)
+		if err != nil {
+			panic(err)
+		}
 	}
+	fmt.Println("Created fake orders.")
+
+	type Order struct {
+		Id          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+
+	var orders []Order
+
+	userID := 1
+	rows, err := db.Query(`
+	SELECT id, user_id, amount, description
+	FROM orders
+	WHERE user_id = $1;`, userID)
 
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("User information: name = %s, email = %s\n", name, email)
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err = rows.Scan(&order.Id, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Orders: ", orders)
 }
