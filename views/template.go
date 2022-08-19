193bad3c7f9a0832b2dc835aab4fdcc08a12bb68
lawrence.li <tiantonl@andrew.cmu.edu>
2022-08-18 22:28:55 -0400

    adding csrf function

--- views/template.go
+++ views/template.go
@@ -6,6 +6,8 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/csrf"
 )
 
 func Must(t Template, err error) Template {
@@ -15,9 +17,16 @@ func Must(t Template, err error) Template {
 	return t
 }
 
-func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
-	tpl, err := template.ParseFS(fs, pattern...)
-
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl := template.New(patterns[0])
+	tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() template.HTML {
+				return `<!-- TODO: Implement csrfField -->`
+			},
+		},
+	)
+	tpl, err := tpl.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing file system template: %w", err)
 	}
@@ -26,27 +35,39 @@ func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
 	}, nil
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
+// func Parse(filepath string) (Template, error) {
+// 	tpl, err := template.ParseFiles(filepath)
 
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
+// 	if err != nil {
+// 		return Template{}, fmt.Errorf("parsing template: %w", err)
+// 	}
 
-	return Template{
-		htmlTpl: tpl,
-	}, nil
-}
+// 	return Template{
+// 		htmlTpl: tpl,
+// 	}, nil
+// }
 
 type Template struct {
 	htmlTpl *template.Template
 }
 
-func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}) {
+	tpl, err := t.htmlTpl.Clone()
+	if err != nil {
+		log.Printf("coning template: %v", err)
+		http.Error(w, "There was an error rendering the page.", http.StatusInternalServerError)
+		return
+	}
+
+	tpl = tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() template.HTML {
+				return csrf.TemplateField(r)
+			},
+		},
+	)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	err := t.htmlTpl.Execute(w, data)
-
+	err = t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("executing template %v", err)
 		http.Error(w, "error occurs while executing template", http.StatusInternalServerError)
